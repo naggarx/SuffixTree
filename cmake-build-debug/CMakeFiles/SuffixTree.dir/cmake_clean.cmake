a3ad8879f4d8aaa275ca809c8fecbc1ea81fc515
naggarx <72861717+naggarx@users.noreply.github.com>
2022-12-10 20:39:51 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SuffixTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SuffixTree.dir/main.cpp.obj"
+  "SuffixTree.exe"
+  "SuffixTree.exe.manifest"
+  "SuffixTree.pdb"
+  "libSuffixTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SuffixTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+class Node
+{
+    Node* child;
+
+    Node()
+    {
+        child = nullptr;
+    }
+    
+
+};
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
